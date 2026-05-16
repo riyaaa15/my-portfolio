ffc00ff978b489a0895727181298a9defcf67a8a
riyaaa15 <riyaredhu1020@gmail.com>
2026-05-16 22:19:05 +0530

    feat: initial project setup and navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import NavBar from './Components/NavBar'
+import Hero from './Components/Hero'
+import About from './Components/About'
+import Skills from './Components/Skills'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 
 function App() {
 
   return (
     <div>
-      
+      <NavBar />
+       <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contacts</li>
+            </ul>
+        </nav>
+    )
+}
